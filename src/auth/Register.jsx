823b15fbd34e1987fe1c19ef39be7a4171a7d2e5
Rohit Gupta <Rohitg8812@gmail.com>
2024-11-18 11:35:29 +0530

    update the register page

--- src/auth/Register.jsx
+++ src/auth/Register.jsx
@@ -39,7 +39,7 @@ function Register() {
             console.log("user Registered successfully")
             toast.success("Registered Successfully")
             setTimeout(() => {
-                navigate("/account")
+                navigate(-1)
                 setLoader(false)
             }, 2000)
         } catch (error) {
